fd90e3a34a1a91bf03cdf11c429293466dac4952
xitiomet <brian@openstatic.org>
2026-03-13 15:28:08 -0400

    added helper script

---
+++ scripts/compress-to-mp3.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+DIR="/opt/recordings/"   # directory passed as argument, default current directory
+
+# This script can be setup as a cronjob to automatically convert your wav files into mp3
+# mp3, ogg, and wav are all supported by the php frontend
+
+find "$DIR" -type f -iname "*.wav" | while IFS= read -r wavfile; do
+    mp3file="${wavfile%.wav}.mp3"
+
+    title=$(exiftool -s3 -Title $wavfile)
+    comment=$(exiftool -s3 -Comment $wavfile)
+    echo "Converting $title: $wavfile -> $mp3file $comment"
+    lame --tt "$title" --tc "$comment" -V2 "$wavfile" "$mp3file" 2> /dev/null
+    status=$?
+
+    if [ $status -eq 0 ]; then
+        rm "$wavfile"
+        echo "Converted and removed original"
+    else
+        echo "Conversion failed: $wavfile"
+        rm -f "$mp3file"
+    fi
+done
